8a698384b3c47041023a46ce8cd5a85c76c1cb8b
CS70 emu <amalpani@cs.hmc.edu>
2017-01-29 20:43:54 -0800

    Update Test.m

--- Test.m
+++ Test.m
@@ -1,1 +1,8 @@
-
+%FInd the impulse response of a function
+fs=10;
+￼t=-1:1/fs:1;
+￼x=cos(2*pi*t); % input signal
+￼y=3*cos(2*pi*t); % output signal
+￼[h,t]=impulse_response(x,y,fs); % we expect the impulse response
+￼amplitude=(1/fs)*trapz(h) % to be a dirac delta with
+￼
